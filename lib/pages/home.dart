ad319682ca3a4d486b6fed6785dbea9cb47c73e0
Tamagochi02 <mirytamara@gmail.com>
2022-09-03 15:38:55 -0500

    home page creation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movil_modular/pages/profile.dart';
 import 'package:movil_modular/widgets/navigation_drawer.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,9 +9,109 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: const NavigationDrawer(),
-      body: const Text("Home"),
-    );
+        appBar: AppBar(
+          title: Text("Gestor Modular"),
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, ProfilePage.route, (route) => false);
+              },
+              icon: Icon(Icons.settings)),
+        ),
+        // drawer: const NavigationDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(17.0),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 51, 51, 51),
+                ),
+                width: double.maxFinite,
+                height: 200,
+                child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Modular I",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        Text(
+                          "0/3 documentos registrados",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        )
+                      ],
+                    )),
+                alignment: Alignment.topLeft,
+              ),
+              SizedBox(height: 17),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 51, 51, 51)),
+                width: double.maxFinite,
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Modulo II",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      Text(
+                        "0/3 documentos registrados",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                alignment: Alignment.topLeft,
+              ),
+              SizedBox(height: 17),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 51, 51, 51)),
+                width: double.maxFinite,
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Modulo III",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      Text(
+                        "0/3 documentos registrados",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                alignment: Alignment.topLeft,
+              ),
+            ],
+          ),
+        ));
   }
 }
