7d44718e26ae1ba3b1058a005f71db0d0da7f022
Tamagochi02 <mirytamara@gmail.com>
2022-09-05 12:25:54 -0500

    added modules pages routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:movil_modular/pages/config.dart';
 import 'package:movil_modular/pages/consultar.dart';
 import 'package:movil_modular/pages/home.dart';
 import 'package:movil_modular/pages/modificar.dart';
+import 'package:movil_modular/pages/moduleOne.dart';
+import 'package:movil_modular/pages/moduleThree.dart';
+import 'package:movil_modular/pages/moduleTwo.dart';
 import 'package:movil_modular/pages/registrar.dart';
 import 'package:movil_modular/pages/profile.dart';
 
@@ -19,11 +21,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         HomePage.route: (context) => const HomePage(),
-        RegisterPage.route: (context) => RegisterPage(),
-        EditPage.route: (context) => const EditPage(),
-        ConsultPage.route: (context) => const ConsultPage(),
-        ConfigPage.route: (context) => const ConfigPage(),
-        ProfilePage.route:(context) => const ProfilePage()
+        // RegisterPage.route: (context) => RegisterPage(),
+        // EditPage.route: (context) => const EditPage(),
+        // ConsultPage.route: (context) => const ConsultPage(),
+        ProfilePage.route:(context) => const ProfilePage(),
+        moduleOnePage.route:(context) => const moduleOnePage(),
+        moduleTwoPage.route:(context) => const moduleTwoPage(),
+        moduleThreePage.route:(context) => const moduleThreePage()
       },
       initialRoute: HomePage.route,
     );
