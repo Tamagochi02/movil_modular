142e89f3b3b72f5f352b0e29ef6b4f441c638d72
Tamagochi02 <mirytamara@gmail.com>
2022-09-07 07:54:44 -0500

    changed page to statelesswidget

--- lib/pages/moduleOne.dart
+++ lib/pages/moduleOne.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:movil_modular/models/routeParamsModularProjectPage.dart';
+import 'package:movil_modular/pages/estadoDelArte.dart';
+import 'package:movil_modular/widgets/doc_card.dart';
 import 'package:movil_modular/widgets/navigation_drawer.dart';
 
-class moduleOnePage extends StatefulWidget {
+class ModuleOnePage extends StatelessWidget {
   static const String route = "/moduleone";
-  const moduleOnePage({Key? key}) : super(key: key);
+  const ModuleOnePage({Key? key}) : super(key: key);
 
-  @override
-  State<moduleOnePage> createState() => _moduleOnePageState();
-}
-
-class _moduleOnePageState extends State<moduleOnePage> {
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments
+        as RouteParamsModularProjectPage;
     return Scaffold(
         appBar: AppBar(
-          title: Text("Modulo I"),
+          title: Text(args.title),
           centerTitle: true,
         ),
         drawer: const NavigationDrawer(),
@@ -24,114 +24,25 @@ class _moduleOnePageState extends State<moduleOnePage> {
           padding: const EdgeInsets.all(17.0),
           child: Column(
             children: [
-//
-              InkWell(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color.fromARGB(255, 51, 51, 51),
-                  ),
-                  width: double.maxFinite,
-                  height: 200,
-                  child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "Estado del Arte",
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          Text(
-                            "Ninguno",
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                          )
-                        ],
-                      )),
-                  alignment: Alignment.topLeft,
-                ),
-                onTap: () {
-                  print("tapped on container");
-                },
-                // onTap: () => Navigator.pushNamedAndRemoveUntil(
-                //     context, moduleOnePage.route, (route) => false),
-              ),
-//
+// Contenedores/cards de documentos en la seccion modulo 1:
+              DocCard(
+                  title: "Estado del Arte",
+                  docName: "Ninguno",
+                  status: DocStatus.sinEntregar),
+
               SizedBox(height: 17),
 //
-              InkWell(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 51, 51, 51)),
-                  width: double.maxFinite,
-                  height: 200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          "Resumen",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 20.0,
-                          ),
-                        ),
-                        Text(
-                          "Ninguno",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  alignment: Alignment.topLeft,
-                ),
-                onTap: () {
-                  print("tapped on container");
-                },
-              ),
-//
+              DocCard(
+                  title: "Resumen",
+                  docName: "Nombre de prueba2",
+                  status: DocStatus.entregado),
+
               SizedBox(height: 17),
 //
-              InkWell(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 51, 51, 51)),
-                  width: double.maxFinite,
-                  height: 200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          "Informe Final",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 20.0,
-                          ),
-                        ),
-                        Text(
-                          "Ninguno",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  alignment: Alignment.topLeft,
-                ),
-                onTap: () {
-                  print("tapped on container");
-                },
-              ),
+              DocCard(
+                  title: "Informe Final",
+                  docName: "Nombre de prueba3",
+                  status: DocStatus.entregado)
 //
             ],
           ),
