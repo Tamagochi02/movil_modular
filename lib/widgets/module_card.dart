d2f4638089acbf998b2aeca4d3392679de89cdaf
Tamagochi02 <mirytamara@gmail.com>
2022-09-13 07:52:39 -0500

    modified pushing route

--- lib/widgets/module_card.dart
+++ lib/widgets/module_card.dart
@@ -14,37 +14,39 @@ class ModuleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Color.fromARGB(255, 51, 51, 51),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color.fromARGB(255, 51, 51, 51),
+          ),
+          width: double.maxFinite,
+          height: 200,
+          child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Text(
+                    "${numDocs}/3 documentos registrados",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  )
+                ],
+              )),
+          alignment: Alignment.topLeft,
         ),
-        width: double.maxFinite,
-        height: 200,
-        child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    fontSize: 20.0,
-                  ),
-                ),
-                Text(
-                  "${numDocs}/3 documentos registrados",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                  ),
-                )
-              ],
-            )),
-        alignment: Alignment.topLeft,
-      ),
-      onTap: () => Navigator.pushNamedAndRemoveUntil(
-          context, ModuleDoc.route, (route) => false, arguments: RouteParamsModularProjectPage(title, '')),
-    );
+        onTap: () {
+          RouteParamsModularProjectPage.assign(title, '');
+          Navigator.pushNamedAndRemoveUntil(
+              context, ModuleDoc.route, (route) => false);
+        });
   }
 }
