bf6242f834290d7aa934fdec4e374b4f78840a5e
Tamagochi02 <mirytamara@gmail.com>
2022-09-14 09:18:47 -0500

    add password field

--- lib/pages/logIn.dart
+++ lib/pages/logIn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:passwordfield/passwordfield.dart';
 
 class LoginPage extends StatefulWidget {
   static const String route = "/login";
@@ -17,14 +18,93 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Color.fromARGB(255, 51, 51, 51),
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: Column(children: [
-          Text(
-            "GESTOR MODULAR",
-            style: TextStyle(color: Colors.white),
-          ),
-          
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          children: [
+            Text(
+              "GESTOR MODULAR",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+            //
+            SizedBox(height: 100),
+            //
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                label: Text(
+                  "Correo institucional:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+            //
+            SizedBox(height: 45),
+            //
+            PasswordField(
+              inputDecoration: PasswordDecoration(
+                inputPadding: const EdgeInsets.symmetric(horizontal: 10),
+                inputStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                ),
+              ),
+            ),
+            //
+            SizedBox(height: 45),
+            //
+            SizedBox(
+              width: 120,
+              height: 30,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Ingresar",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    )),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+            ),
+            //
+            SizedBox(height: 45),
+            //
+            Divider(
+              color: Colors.white,
+            ),
+            //
+            SizedBox(height: 45),
+            //
+            SizedBox(
+              width: 170,
+              height: 30,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Crear una cuenta",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    )),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
         ),
       ),
     );
