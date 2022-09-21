2d68dbd58a0441a974a2abb964f557dacf69a847
Tamagochi02 <mirytamara@gmail.com>
2022-09-21 09:31:16 -0500

    changed stateful widget to stateless

--- lib/pages/registerUser.dart
+++ lib/pages/registerUser.dart
@@ -2,40 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:movil_modular/pages/logIn.dart';
+import 'package:movil_modular/pages/registerStudent.dart';
 
-class RegisterUserPage extends StatefulWidget {
+class RegisterUserPage extends StatelessWidget {
   static const String route = "/registeruser";
   const RegisterUserPage({Key? key}) : super(key: key);
 
   @override
-  State<RegisterUserPage> createState() => _RegisterUserPageState();
-}
-
-class _RegisterUserPageState extends State<RegisterUserPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 51, 51, 51),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pushNamedAndRemoveUntil(
-                  context, LoginPage.route, (route) => false);
-            },
-            icon: Icon(Icons.arrow_back)),
-      ),
       body: Padding(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.symmetric(horizontal: 7, vertical: 40),
         child: Column(
           children: [
-            SizedBox(
-              width: 300,
-              height: 50,
-              child: ElevatedButton(onPressed: () {}, child: Text("Alumno")),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, LoginPage.route, (route) => false);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 240),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                          context, RegisterStudentPage.route);
+                        },
+                        child: Text("Alumno"),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 68, 117, 223),
+                        )),
+                  ),
+                  //
+                  SizedBox(height: 45),
+                  //
+                  SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Docente"),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 170, 96, 219),
+                        )),
+                  )
+                ],
+              ),
             )
           ],
-          mainAxisAlignment: MainAxisAlignment.center,
         ),
       ),
     );
